2700aafb02624aad79cadeb6f9c1ff3b20e181dd
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-25 22:02:17 +0530

    Add UserDetailService for custom user authentication; include JWT dependencies in pom.xml

---
+++ springJwt/src/main/java/lk/ijse/springJwt/service/UserDetailService.java
@@ -0,0 +1,21 @@
+package lk.ijse.springJwt.service;
+
+import lk.ijse.springJwt.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+}
