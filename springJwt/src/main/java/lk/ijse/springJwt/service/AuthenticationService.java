ff8d9989f198a9bc63aeb520c7a46f07805e945b
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-26 12:50:51 +0530

    Add AuthenticationService and AuthenticationResponse for user registration and authentication

---
+++ springJwt/src/main/java/lk/ijse/springJwt/service/AuthenticationService.java
@@ -0,0 +1,50 @@
+package lk.ijse.springJwt.service;
+
+import lk.ijse.springJwt.model.AuthenticationResponse;
+import lk.ijse.springJwt.model.User;
+import lk.ijse.springJwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(User request){
+        User user = new User();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        user.setRole(request.getRole());
+
+        user = userRepository.save(user);
+
+        String token = jwtService.generateToken(user);
+
+        return new AuthenticationResponse(token);
+    }
+
+    public AuthenticationResponse authenticate(User request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        String token = jwtService.generateToken(user);
+
+        return new AuthenticationResponse(token);
+    }
+}
