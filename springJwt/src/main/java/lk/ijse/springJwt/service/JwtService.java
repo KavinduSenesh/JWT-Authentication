8b7cced1dc81363baa993ea014743a5d8fa63e8f
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-26 13:03:50 +0530

    Remove hardcoded SECRET_KEY from JwtService for improved security

--- springJwt/src/main/java/lk/ijse/springJwt/service/JwtService.java
+++ springJwt/src/main/java/lk/ijse/springJwt/service/JwtService.java
@@ -15,7 +15,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private final String SECRET_KEY = "a3f8e4b3004f10691346553f87afaaeddf77751918dbca6afa72bed72b0ea0b8ad043a0efb4a8738d200157b3d3a6c82265660e15cf8a26cffe4111d2f22312bbbb24ffbf984a30ceea6b53eafc85d590ff49ffbbe4f94c894f8e3483b1c8f5e4db1951af2db300411049852b52db8d594de3f4874336d155eb3e3c24750d0127d2829a5b938dc76f5c31de5eb12cd4284b0e772b338cad0ad5fa1c720b98da0b3d1c180ed0899b74acf32cdc7593a8044d89f9fcbde7c9a177646b140dc59c471866510c9de68cdd8f48c2646e28b7230ce70f683d6639300c72ce87e87cf5b909bc45d63ca54d2b2534dfaea9857377203ea9df759448f1bd40e9b3268e498";
+    private final String SECRET_KEY = "";
 
     public String extractUsername(String token) {
         return extractClaims(token, Claims::getSubject);
